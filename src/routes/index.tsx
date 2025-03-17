ebeb5f97534815412edc08be2e290cbf302470ef
ppnathaniel <patricianphillip@gmail.com>
2025-03-17 10:13:41 -0500

    initial commit

---
+++ src/routes/index.tsx
@@ -0,0 +1,38 @@
+import * as React from "react";
+import { Navigate, type RouteObject } from "react-router-dom";
+
+import { Page as NotFoundPage } from "@/pages/not-found";
+
+import { route as dashboardRoute } from "./dashboard";
+
+export const routes: RouteObject[] = [
+	{ index: true, element: <Navigate to="/dashboard" /> },
+	{
+		path: "errors",
+		children: [
+			{
+				path: "internal-server-error",
+				lazy: async () => {
+					const { Page } = await import("@/pages/errors/internal-server-error");
+					return { Component: Page };
+				},
+			},
+			{
+				path: "not-authorized",
+				lazy: async () => {
+					const { Page } = await import("@/pages/errors/not-authorized");
+					return { Component: Page };
+				},
+			},
+			{
+				path: "not-found",
+				lazy: async () => {
+					const { Page } = await import("@/pages/errors/not-found");
+					return { Component: Page };
+				},
+			},
+		],
+	},
+	dashboardRoute,
+	{ path: "*", element: <NotFoundPage /> },
+];
